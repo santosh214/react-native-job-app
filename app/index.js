a8182bff46eca14c6a03598333c9d496cd3a6e9f
santosh214 <santoshsingh74827@gmail.com>
2023-08-10 18:32:20 +0530

    add cli cmd and make screenheader btn

--- app/index.js
+++ app/index.js
@@ -1,13 +1,35 @@
-import {View, ScrollView, SafeAreaView} from 'react-native';
+import {View, ScrollView, SafeAreaView, Text} from 'react-native';
 import {Stack, useRouter} from 'expo-router';
 import {useState} from 'react';
 import {About, Contact} from '../components';
+import ScreenHeaderBtn from '../components/HeaderButtons/ScreenHeaderBtn';
 
 const Home = () => {
   const router = useRouter();
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'skyblue'}}>
-      <Text>Home</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <Stack.Screen
+        options={{
+          headerStyle: {backgroundColor: 'skyblue'},
+          headerShadowVisible: false,
+          headerLeft: () => {
+            <ScreenHeaderBtn />;
+          },
+          headerTitle: ''
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: 2,
+            backgroundColor: 'red',
+            alignItems: 'center'
+          }}
+        >
+          <Text style={{fontSize: 30}}>Home</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ components/HeaderButtons/ScreenHeaderBtn.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ScreenHeaderBtn = () => {
+  return (
+    <View>
+      <Text>ScreenHeaderBtn</Text>
+    </View>
+  );
+};
+
+export default ScreenHeaderBtn;
